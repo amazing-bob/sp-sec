8d097859a383193aaa5270599150efa1b7dd8626
Sangheon <buru1020@gmail.com>
2023-06-20 14:04:19 +0900

    Custom Login Filter

---
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.sp.fc.web.config;
+
+import com.sp.fc.web.student.StudentManager;
+import com.sp.fc.web.teacher.TeacherManager;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private StudentManager studentManager;
+    private TeacherManager teacherManager;
+
+    public SecurityConfig(StudentManager studentManager, TeacherManager teacherManager) {
+        this.studentManager = studentManager;
+        this.teacherManager = teacherManager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(studentManager);
+        auth.authenticationProvider(teacherManager);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CustomLoginFilter loginFilter = new CustomLoginFilter(authenticationManager());
+        http
+                .authorizeRequests(request->
+                        request.antMatchers("/", "/login").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(login ->
+                        login.loginPage("/login")
+                                .permitAll()
+                                .defaultSuccessUrl("/", false)
+                                .failureUrl("/login-error")
+                )
+                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"))
+                ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                ;
+    }
+}
