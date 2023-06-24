9581cd810bf1eee0d8dcdd6affd76eb0a9d097d4
Sangheon <buru1020@gmail.com>
2023-06-24 20:54:45 +0900

    DaoAuthentication UserDetailsService

---
+++ comp/user-admin/src/main/java/com/sp/fc/user/domain/SpAuthority.java
@@ -0,0 +1,27 @@
+package com.sp.fc.user.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.lang.annotation.Target;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="sp_user_authority")
+@IdClass(SpAuthority.class)
+public class SpAuthority implements GrantedAuthority {
+
+    @Id
+    @Column(name = "user_id")
+    private Long userId;
+    @Id
+    private String authority;
+}
